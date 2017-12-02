a917adb507f6b76086d2bb4f804b3fce4151eec5
Chris Johnson <crjjrc@gmail.com>
2017-12-02 08:57:15 -0600

    Add u or ru verb quizzer.

---
+++ japanese.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+
+require 'set'
+
+class Verb
+  attr_reader :hiragana, :definition
+
+  E_HIRAGANA = Set.new %w{え け せ て ね へ め れ げ ぜ べ ぺ}
+  I_HIRAGANA = Set.new %w{い き し ち に ひ み り ぎ じ び ぴ}
+
+  def initialize d
+    @properties = d
+    @hiragana = d['hiragana']
+    @definition = d['definition']
+  end
+
+  def is_ru?
+    (@properties.has_key?('type') && @properties['type'] == 'ru') ||
+    (hiragana[-1] == 'る' && (E_HIRAGANA.member?(hiragana[-2]) || I_HIRAGANA.member?(hiragana[-2])))
+  end
+
+  def is_u?
+    (@properties.has_key?('type') && @properties['type'] == 'u') || !is_ru?
+  end
+end
